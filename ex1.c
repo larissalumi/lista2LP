82f09491f2f28a733b2a4f7611cf00e077e31dcf
larissalumi <larissalumih@gmail.com>
2024-08-24 21:10:29 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ int main () {
+    int vetor[19], x, exist=0;
+    for(int i = 0; i < 19; i++){
+        printf("Digite o %do numero: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+    printf("Digite o numero que voce quer buscar:");
+    scanf("%d", &x);
+
+    for(int i = 0; i < 19; i++){
+        if(vetor[i] == x && exist == 0){
+            printf("O seu numero existe em:\n   %d  |  ", i+1);
+            exist++;
+            continue;
+        }
+        if(vetor[i] == x){
+            printf("%d  |  ", i+1);
+            exist++;
+        }
+    }
+    if(exist==0){
+        printf("Valor nao encontrado");
+    }
+ }
