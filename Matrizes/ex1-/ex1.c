a0efa97409e9fe4e67d9c8b35ad76f3fc63a76d2
Larissa Lumi <larissalumih@gmail.com>
2024-09-08 23:18:38 -0300

    ex1 vetor

---
+++ Matrizes/ex1-/ex1.c
@@ -0,0 +1,32 @@
+/*Ler uma matriz m de números inteiros com 20x20 posições. Ler mais um número inteiro x. Buscar o número x na matriz m e imprimir todas as posições em que ele foi encontrado. Caso o número x não seja encontrado, imprimir a mensagem “Valor não encontrado”.
+*/
+
+#include <stdio.h>
+
+int main () {
+    int matriz[5][5], n, existe = 0;
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            printf("Digite o numero na posicao %dX%d: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("Digite o numero que voce deseja encontrar: ");
+    scanf("%d", &n);
+
+    printf("Encontrando as posicoes com o numero %d...\n", n);
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(matriz[i][j] == n){
+                printf("%dX%d\n", i, j);
+                existe = 1;
+            }
+        }
+    }
+    if(existe != 1){
+        printf("O numero %d nao foi encontrado.", n);
+    }
+    return 0;
+}
