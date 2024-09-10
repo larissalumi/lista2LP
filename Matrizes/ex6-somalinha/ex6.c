c4d1e175e0c2c377412a50a57f73a0e8bb41c51e
Larissa Lumi <larissalumih@gmail.com>
2024-09-10 17:01:06 -0300

    ex6

---
+++ Matrizes/ex6-somalinha/ex6.c
@@ -0,0 +1,37 @@
+/*Dada uma matriz MAT de inteiros com 4 x 5 elementos, 
+faça um algoritmo para somar os elementos de cada linha gerando o vetor SOMALINHA. 
+Em seguida, some os valores dos elementos do vetor em uma variável TOTAL, 
+que deve ser impressa ao final.
+*/
+
+#include <stdio.h>
+
+int main () {
+    int mat[4][5], somalinha[5], aux = 0, total = 0;
+
+    //nomear elementos
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 5; j++){
+            printf("Digite um valor %dX%d: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    //somalinha
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 5; j++){
+            aux += mat[i][j]; 
+        }
+        somalinha[i] = aux;
+        aux = 0;
+    }
+
+    //total
+    for(int i = 0; i < 4; i++){
+        total += somalinha[i];
+        printf("%d\n", somalinha[i]);
+    }
+
+    //display:
+    printf("Total: %d", total);
+}
