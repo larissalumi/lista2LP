d03cc12c9c726e74eebd01ff38d889c7d16caf4f
Larissa Lumi <larissalumih@gmail.com>
2024-09-11 16:36:09 -0300

    ex1 recursividade

---
+++ Recursividade/ex1-fibonacci/ex1.c
@@ -0,0 +1,25 @@
+/*Faça uma função recursiva que receba um inteiro N como parâmetro e retorne o N-ésimo
+ termo da sequência de Fibonacci.
+*/
+
+#include <stdio.h>
+
+int fibo(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main(){
+    int n, nTermo;
+    printf("Digite o n-esimo termo de fibonacci desejado: ");
+    scanf("%d", &n);
+
+    nTermo = fibo(n);
+
+    printf("O %do termo e %d.\n", n, nTermo);
+}
